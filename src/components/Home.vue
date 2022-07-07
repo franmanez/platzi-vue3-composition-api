155ac1112b743e104fb06b9354a9168af273628e
Fran <francisco.manez@upc.edu>
2022-07-07 10:23:40 +0200

    initial commit: composition API

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Composition API</div>
+</template>
+
+<script>
+
+import { onMounted } from 'vue'
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Home',
+  setup (props, ctx) {
+    onMounted(() => {
+      console.log('mounted')
+    })
+  }
+}
+</script>
